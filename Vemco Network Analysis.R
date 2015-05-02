f7b956d265b0347e2f7227c2e991b01915ebb8d2
gregorylburgess <gregory.l.burgess@gmail.com>
2015-05-01 16:28:30 -1000

    Added crappy graphing.

--- Vemco Network Analysis.R
+++ Vemco Network Analysis.R
@@ -356,12 +356,12 @@ analysis4 = function() {
   # F = graph.maxflow(graph, source, target, capacity=NULL) //gives max flow from source to target
   # k = vertex.connectivity(graph, source=NULL, target=NULL, checks=TRUE)
   # F/k = importance of edge from a to b
-  # edges with high F/k are important
+  # edges with high F/k are importantw
   
   graph = get_graph(vue_data,igraph=T,binary=F,removeLoops=T)
   result = matrix(,numStations,numStations);
   for(source in 1:numStations) {
-    for(target in 1:1) {
+    for(target in 1:numStations) {
       if(source!=target && graph[source,target]==0) {
         flow = graph.maxflow(graph, source, target, capacity=E(graph)$weight)
         print(flow$value)
@@ -435,8 +435,80 @@ runall = function() {
   return(rslt)
 }
 
+graphall = function(rslt){
+  
+  myImagePlot(rslt$timePath,title=c("Temporally Enduring Paths"))
+  myImagePlot(rslt$edgeImp,title=c("Important Edges (Flow/#paths)"))
+  myImagePlot(rslt$speciesPath,title=c("Species Specific Paths"))
+}
 
-
+# ----- Define a function for plotting a matrix ----- #
+myImagePlot <- function(x, ...){
+  min <- min(x)
+  max <- max(x)
+  yLabels <- rownames(x)
+  xLabels <- colnames(x)
+  title <-c()
+  # check for additional function arguments
+  if( length(list(...)) ){
+    Lst <- list(...)
+    if( !is.null(Lst$zlim) ){
+      min <- Lst$zlim[1]
+      max <- Lst$zlim[2]
+    }
+    if( !is.null(Lst$yLabels) ){
+      yLabels <- c(Lst$yLabels)
+    }
+    if( !is.null(Lst$xLabels) ){
+      xLabels <- c(Lst$xLabels)
+    }
+    if( !is.null(Lst$title) ){
+      title <- Lst$title
+    }
+  }
+  # check for null values
+  if( is.null(xLabels) ){
+    xLabels <- c(1:ncol(x))
+  }
+  if( is.null(yLabels) ){
+    yLabels <- c(1:nrow(x))
+  }
+  
+  layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(4,1), heights=c(1,1))
+  
+  # Red and green range from 0 to 1 while Blue ranges from 1 to 0
+  ColorRamp <- rgb( seq(0,1,length=256),  # Red
+                    seq(0,1,length=256),  # Green
+                    seq(1,0,length=256))  # Blue
+  ColorLevels <- seq(min, max, length=length(ColorRamp))
+  
+  # Reverse Y axis
+  reverse <- nrow(x) : 1
+  yLabels <- yLabels[reverse]
+  x <- x[reverse,]
+  
+  # Data Map
+  par(mar = c(3,5,2.5,2))
+  image(1:length(xLabels), 1:length(yLabels), t(x), col=ColorRamp, xlab="",
+        ylab="", axes=FALSE, zlim=c(min,max))
+  if( !is.null(title) ){
+    title(main=title)
+  }
+  axis(BELOW<-1, at=1:length(xLabels), labels=xLabels, cex.axis=0.7)
+  axis(LEFT <-2, at=1:length(yLabels), labels=yLabels, las= HORIZONTAL<-1,
+       cex.axis=0.7)
+  
+  # Color Scale
+  par(mar = c(3,2.5,2.5,2))
+  image(1, ColorLevels,
+        matrix(data=ColorLevels, ncol=length(ColorLevels),nrow=1),
+        col=ColorRamp,
+        xlab="",ylab="",
+        xaxt="n")
+  
+  layout(1)
+}
+# ----- END plot function ----- #
 
 
 
